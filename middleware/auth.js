dd8178f3f4d9923589573c26d08770fdb299557f
rohitkushwaha8 <rohitkushwaha8962658973@gmail.com>
2026-05-29 10:16:32 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+function verifyAdmin(req, res, next) {
+  const pwd = req.headers['x-admin-password'] || req.body?.adminPassword;
+  if (!pwd || pwd !== process.env.ADMIN_PASSWORD) {
+    return res.status(401).json({ success: false, message: 'Wrong admin password' });
+  }
+  next();
+}
+
+module.exports = { verifyAdmin };
